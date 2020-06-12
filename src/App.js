4f9ec9de1d7ca201c4b2c824e7bc6760110ebcf2
Alex Dollard <alexdollard141@gmail.com>
2020-06-12 09:08:22 -0500

    beautiful approval cards

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import Header from  './components/Header';
 import Comment from  './components/Comment'
 import faker from 'faker'; 
+import ApprovalCard from './components/ApprovalCard'
 
 function App() {
   return (
-    <div class="ui container comments">
-      <Header />
-      <Comment author="Ben" timeAgo="today at 6:12am" content="bullshit!" avatar={faker.image.avatar()}/>
-      <Comment author="Stevie" timeAgo="today at 10:45am" content="Hello I have two friends" avatar={faker.image.avatar()}/>
-      <Comment author="Horace" timeAgo="today at 12:22pm" content="blueberry pancakes." avatar={faker.image.avatar()}/>
+    <div className="ui container comments">
+      <ApprovalCard >
+        <Comment author="Ben" timeAgo="today at 6:12am" content="bullshit!" avatar={faker.image.avatar()}/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comment author="Stevie" timeAgo="today at 10:45am" content="Hello I have two friends" avatar={faker.image.avatar()}/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comment author="Horace" timeAgo="today at 12:22pm" content="blueberry pancakes." avatar={faker.image.avatar()}/>
+      </ApprovalCard>
     </div> 
   );
 }
